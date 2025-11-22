6e846282064aa004d56bd2d3cfc8410c587625ba
josecabellog <josegregoriocabellog@gmail.com>
2025-11-22 17:32:05 -0400

    functions

---
+++ js/06-functions.js
@@ -0,0 +1,23 @@
+
+
+// funciones en JavaScript 
+
+// declaracion de una funcion 
+function greet() {
+    console.log("Hello!")
+}
+
+// invocacion de una funcion
+console.log(greet())
+
+// funcion flecha 
+const greetArrow = () => {
+    console.log("Hello!")
+}
+console.log(greetArrow())
+
+//funcion expresion 
+const greetExpression = function() {
+    console.log("Hello!")
+}
+console.log(greetExpression())
