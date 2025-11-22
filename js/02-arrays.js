729128ba7f3e6e44d3ca36ba85fea9c565fc4c0c
josecabellog <josegregoriocabellog@gmail.com>
2025-11-22 16:37:57 -0400

    segundo commit

--- js/02-arrays.js
+++ js/02-arrays.js
@@ -1,2 +1,12 @@
 
 
+/// Arrays in JavaScript 
+fruits = ["apple", "banana", "cherry"]
+/// console.log(fruits)
+
+fruits.push("orange");
+console.log(fruits)
+
+console.log(fruits[1])
+
+console.log(fruits.length)
