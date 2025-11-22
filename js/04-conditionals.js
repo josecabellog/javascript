cdef733ff9dc80e60bd21bd9f2a6914a89745a55
josecabellog <josegregoriocabellog@gmail.com>
2025-11-22 17:10:33 -0400

    conditionals

---
+++ js/04-conditionals.js
@@ -0,0 +1,13 @@
+
+
+/// Conditionals in JavaScript
+
+let age = 65
+
+if (age >= 18) {
+    console.log("You're an adult");
+}   else if (age >=65) {
+    console.log("you're a senior");
+}   else {
+    console.log("You're a minor");
+}
