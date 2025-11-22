412e48641b47e4ebc38fa2e57da8af4ee18c692b
josecabellog <josegregoriocabellog@gmail.com>
2025-11-22 16:55:23 -0400

    tercer commit

---
+++ js/03-objects.js
@@ -0,0 +1,13 @@
+
+
+const person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+
+console.log(person.name)
+
+hobbies = ["reading", "traveling", "sports"]
+
+console.log(person.name, hobbies[1])
