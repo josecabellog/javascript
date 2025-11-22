998a41f453b1b2a2f7c5118c335ed2502334918a
josecabellog <josegregoriocabellog@gmail.com>
2025-11-22 17:19:36 -0400

    for_loops

---
+++ js/05-for_loops.js
@@ -0,0 +1,20 @@
+
+
+// bucles in JavaScript
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+// Recorrer un arreglo 
+
+let colors = ["red", "green", "blue"];
+for (let i = 0; i < colors.length; i++) {
+    console.log(colors[i]);
+}
+
+colors.push("yellow")
+
+console.log(colors)
+
+colors.push("purple")
+console.log(colors)
